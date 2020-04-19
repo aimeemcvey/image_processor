45d8046a88221ad27a6295635c5b3959bf90f22f
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-19 13:05:07 -0400

    Add upload new button selection

--- image_processor_client.py
+++ image_processor_client.py
@@ -30,20 +30,20 @@ def main_window():
 
     # Add Radiobuttons
     action = StringVar()
-    ttk.Radiobutton(root, text="Upload", variable=action,
-                    value="upload").grid(column=0, row=2, sticky=W)
     ttk.Radiobutton(root, text="Invert", variable=action,
-                    value="invert").grid(column=0, row=3, sticky=W)
+                    value="invert").grid(column=0, row=2, sticky=W)
     ttk.Radiobutton(root, text="Display", variable=action,
-                    value="display").grid(column=0, row=4, sticky=W)
+                    value="display").grid(column=0, row=3, sticky=W)
     ttk.Radiobutton(root, text="Download", variable=action,
-                    value="download").grid(column=0, row=5, sticky=W)
+                    value="download").grid(column=0, row=4, sticky=W)
 
     # Add buttons
     ok_btn = ttk.Button(root, text="Ok")  # command=ok_button)
-    ok_btn.grid(column=0, row=6, columnspan=2)
+    ok_btn.grid(column=0, row=6)
     cancel_btn = ttk.Button(root, text="Cancel", command=cancel_button)
-    cancel_btn.grid(column=2, row=6)
+    cancel_btn.grid(column=1, row=6)
+    upload_btn = ttk.Button(root, text="Upload New")  # command=cancel_button)
+    upload_btn.grid(column=2, row=6)
 
     root.mainloop()
     return

0fcc418dcbe6fca7c3a7082f892bbc5ae0610a9b
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-18 20:33:42 -0400

    Create main window (#20)

---
+++ image_processor_client.py
@@ -0,0 +1,15 @@
+# image_processor_client.py
+from tkinter import *
+from tkinter import ttk
+
+
+def main_window():
+    root = Tk()  # sets up main window
+    root.title("Image Processor")
+
+    root.mainloop()
+    return
+
+
+if __name__ == "__main__":
+    main_window()

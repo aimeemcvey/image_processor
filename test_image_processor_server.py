4d06d0b84afbf78164afc78cfed4bffc26b1d889
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-20 12:13:50 -0400

    Fix pep-8 errors

--- test_image_processor_server.py
+++ test_image_processor_server.py
@@ -5,7 +5,8 @@ import pytest
 @pytest.mark.parametrize("in_dict, expected", [
     ({"image": "acl1.jpg", "b64_string": ""}, True),
     ({"imge": "acl1.jpg", "b64_string": ""}, "image key not found"),
-    ({"image": "acl1.jpg", "b64_string": 673241}, "b64_string value not a string"),
+    ({"image": "acl1.jpg", "b64_string": 673241},
+     "b64_string value not a string"),
 ])
 def test_verify_image_info(in_dict, expected):
     from image_processor_server import verify_image_info

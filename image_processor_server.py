7a587e9ab4df14c921f211a2577f7946a584d9f9
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-19 23:55:03 -0400

    Add function to add image to db

--- image_processor_server.py
+++ image_processor_server.py
@@ -5,16 +5,17 @@ from datetime import datetime
 import requests
 from pymodm import connect, MongoModel, fields
 
-connect("mongodb+srv://db_access:swim4life@aimeemcv-7rfsl.mongodb.net/imagedb?retryWrites=true&w=majority")
+connect("mongodb+srv://db_access:swim4life@aimeemcv-7rfsl.mongodb.net/"
+        "imagedb?retryWrites=true&w=majority")
 app = Flask(__name__)
 
 
 class Image(MongoModel):
     image_name = fields.CharField(primary_key=True)
-    image_formats = fields.ListField(field=fields.DictField())
-    upload_time = fields.DateTimeField
-    image_size = fields.ListField(field=fields.DictField())
-    processed_info = fields.ListField(field=fields.DictField())
+    image_formats = fields.DictField()
+    upload_time = fields.CharField()
+    # image_size = fields.ListField()
+    # processed_info = fields.ListField()
 
 
 @app.route("/api/upload_image", methods=["POST"])
@@ -23,10 +24,11 @@ def post_new_image():
     check_result = verify_image_info(in_dict)
     if check_result is not True:
         return check_result, 400
+    # verify it's actually an image w correct extension and route
     # if is_image_in_database(in_dict["image_name"]) is True:
     #     return "Image {} has already been added to server" \
     #                .format(in_dict["image_name"]), 400
-    # add_image_to_db(in_dict["image_name"])
+    add_image_to_db(in_dict)
     return "Image added", 200
 
 
@@ -38,6 +40,15 @@ def verify_image_info(in_dict):
             return "{} key not found".format(key)
         if type(in_dict[key]) is not expected_types[i]:
             return "{} value not a string".format(key)
+    return True
+
+
+def add_image_to_db(in_dict):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    new_image = Image(image_name=in_dict["image"],
+                      image_formats={"b64_str": in_dict["b64_string"]},
+                      upload_time=timestamp)
+    new_image.save()
     return True
 
 

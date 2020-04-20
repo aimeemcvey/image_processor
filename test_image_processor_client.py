2bc521bea1bae14e065c9e363e814b631ef7e9e4
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-20 13:28:51 -0400

    Adds unit test for if wrong file type upload attempt

--- test_image_processor_client.py
+++ test_image_processor_client.py
@@ -13,3 +13,10 @@ def test_image_file_to_b64_notfound():
     answer = image_file_to_b64("images/acl.jpg")
     expected = False
     assert answer == expected
+
+
+def test_image_file_to_b64_wrongfiletype():
+    from image_processor_client import image_file_to_b64
+    answer = image_file_to_b64("images/test.txt")
+    expected = ""
+    assert answer == expected

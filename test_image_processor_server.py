9794618d25a9cf6b83d9a559d60f81bfacb322e5
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-20 21:48:56 -0400

    Add false unit test for is_image_in_db

--- test_image_processor_server.py
+++ test_image_processor_server.py
@@ -14,6 +14,20 @@ def test_verify_image_info(in_dict, expected):
     assert answer == expected
 
 
+def test_is_image_in_database_true():
+    from image_processor_server import is_image_in_database
+    answer = is_image_in_database("acl100.jpg")
+    expected = True
+    assert answer == expected
+
+
+def test_is_image_in_database_false():
+    from image_processor_server import is_image_in_database
+    answer = is_image_in_database("acl99.jpg")
+    expected = False
+    assert answer == expected
+
+
 def test_add_image_to_db():
     from image_processor_server import add_image_to_db
     in_dict = {'image': 'acl100.jpg', 'b64_string': "3lkewar90eq3ljafjdl"}

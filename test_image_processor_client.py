e2eee22ab77c25b3604a25b6d722f6ebb2b4e252
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-24 14:31:34 -0400

    Create unit test for b64_string_to_ndarray

--- test_image_processor_client.py
+++ test_image_processor_client.py
@@ -2,6 +2,29 @@
 import pytest
 
 
+def test_b64_string_to_ndarray():
+    from image_processor_client import image_file_to_b64
+    from image_processor_client import b64_string_to_ndarray
+    b64 = image_file_to_b64("images/acl1_test.jpg")
+    nd = b64_string_to_ndarray(b64)
+    answer = nd[25][0:5]
+    expected = [[5, 5, 5],
+                [5, 5, 5],
+                [5, 5, 5],
+                [5, 5, 5],
+                [5, 5, 5]]
+    assert (answer == expected).all
+#
+#
+# def display_image(img_ndarray):
+#     try:
+#         plt.imshow(img_ndarray, interpolation="nearest")
+#         plt.show()
+#     except Error:
+#         return False
+#     return True
+
+
 def test_image_file_to_b64_exists():
     from image_processor_client import image_file_to_b64
     answer = image_file_to_b64("images/acl1.jpg")

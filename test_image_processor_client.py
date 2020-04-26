5e02e4dbf4897387a254de93d869cf78641b2175
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-26 19:16:16 -0400

    Remove unused display_image function

--- test_image_processor_client.py
+++ test_image_processor_client.py
@@ -16,17 +16,6 @@ def test_b64_string_to_ndarray():
     assert (answer == expected).all
 
 
-def test_display_image():
-    from image_processor_client import image_file_to_b64
-    from image_processor_client import b64_string_to_ndarray
-    from image_processor_client import display_image
-    b64 = image_file_to_b64("images/acl1_test.jpg")
-    nd_array = b64_string_to_ndarray(b64)
-    answer = display_image(nd_array)
-    expected = True
-    assert answer == expected
-
-
 def test_create_filename():
     from image_processor_client import create_filename
     answer = create_filename("acl1.jpg")
